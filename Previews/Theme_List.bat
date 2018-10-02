fb5f4b6cf6d9cfaaf877b8fb7a4413caf80402db
Dawson <NFLD99@users.noreply.github.com>
2018-10-02 02:02:52 -0230

    Updated
    Fixed file url

--- Previews/Theme_List.bat
+++ Previews/Theme_List.bat
@@ -1,18 +1,8 @@
 @echo off
-echo Starting...
-Set "url=https://nfld99.com/assets/NFLD99.txt"
-for %%# in (%url%) do ( set "File=%tmp%\%%~n#.txt" )
-Call :Download "%url%" "%File%"
-If exist "%File%" Type "%File%"
-:Download <url> <File>
-Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%1','%2')"
-forfiles /S /M *.PNG /C "cmd /c rename @file @fname.png"
+cd C:\Users\Dawso\Music\Github-Repos\Better-Discord\Requested\Updated
 del "Theme_List.txt" >nul 2>&1
-for /D %%A IN ("*") DO echo "%%A">>Theme_List.txt
+for /s %%A IN ("*") DO echo "%%A">>Theme_List.txt
 cls
 echo Finished.
 echo Window will close in 10s, Or press any key to close now.
-If exist "%File%" Type "%File%"
-del "NULs" >nul 2>&1
-@TIMEOUT /T 10 > NUL & Exit 
-exit /b
+@TIMEOUT /T 10 > NUL
