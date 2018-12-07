8cf4c33f5314297c900db063c0b36dcb9e911046
Dawson <NFLD99@users.noreply.github.com>
2018-12-07 03:07:49 -0330

    Updated
    Change log...ment to push this like 4 days ago lol

--- Requested/Updated/List.bat
+++ Requested/Updated/List.bat
@@ -1,5 +1,5 @@
 @echo off
-findstr "Chat-Background" *.theme.css >>C:\Users\Dawso\Music\Github-Repos\Better-Discord\List.txt
+findstr "root" *.theme.css >>C:\Users\Dawso\Music\Github-Repos\Better-Discord\List.txt
 cls
 echo Finished.
 @TIMEOUT /T 1 > NUL
