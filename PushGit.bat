c38da7df68e16f3cfdd5a67fc514870d21c319c7
Dawson <NFLD99@users.noreply.github.com>
2019-05-21 19:58:18 -0230

    Updated

--- PushGit.bat
+++ PushGit.bat
@@ -1,4 +1,2 @@
 cd C:\Users\Dawso\Music\Github-Repos\Better-Discord
-"c:\Program Files\Git\bin\sh.exe" --login -i -c "git commit -am "Updated""
-timeout /t 10
 "c:\Program Files\Git\bin\sh.exe" --login -i -c "git push"
