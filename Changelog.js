d9fc2382cee3b3af47e3b12d819de8d828fd16d1
Dawson <NFLD99@users.noreply.github.com>
2019-05-26 11:08:56 -0230

    Updated

--- Changelog.js
+++ Changelog.js
@@ -1,8 +1,6 @@
 var pathInput = "C:/Users/Dawso/Music/Github-Repos/Better-Discord/Source_Code/Master_Code.css";
 var pathTempOutput = "C:/Users/Dawso/Music/Github-Repos/temp.txt"
 var pathOutput = "C:/Users/Dawso/Music/Github-Repos/Better-Discord/ChangeLog.txt"
-
-const _fs = require("fs");
 var text = _fs.readFileSync(pathInput, "utf8");
 var match = null, rx = /(?:__(.*?)__.*?)+/g, res = [];
 while(match = rx.exec(text)) res.push(match[1]);
