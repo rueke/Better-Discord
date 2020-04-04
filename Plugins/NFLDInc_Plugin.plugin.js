8acb5044bab6062b51fa45aa30341ad17aa753b2
Dawson <NFLD99@users.noreply.github.com>
2020-04-04 03:50:27 -0230

    Updated

--- Plugins/NFLDInc_Plugin.plugin.js
+++ Plugins/NFLDInc_Plugin.plugin.js
@@ -3,70 +3,68 @@
 class NFLDInc_Plugin {
   getName() { return "NFLDInc_Plugin"; }
 
-  getVersion() { return "2.0.0"; }
+  getVersion() { return "4.0.0"; }
 
   getAuthor() { return "Nfld99"; }
 
   getDescription() { return "NFLDInc_Plugin."; }
 
   constructor() {
-    this.patches = [];
+    this.addElement = this.addElement.bind(this);
   }
-
-  //legacy
-  load() { }
 
   start() {
-    ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin","2.0.0","https://raw.githubusercontent.com/NFLD99/Better-Discord/master/Plugins/NFLDInc_Plugin.plugin.js")
-    // add installed tag to appmount
-    var ele = document.getElementById("app-mount").hasAttribute("NFLDInc");
-    if (ele == false) {
-      document.getElementById("app-mount").setAttribute("NFLDInc", "Installed");
-    }
+    // If ZLib exists, check for updates
+    if (window.ZLibrary) ZLibrary.PluginUpdater.checkForUpdate("NFLDInc_Plugin","3.0.0","https://raw.githubusercontent.com/NFLD99/Better-Discord/master/Plugins/NFLDInc_Plugin.plugin.js")
 
-    // Add current user to appmount
-    var CUser = document.getElementsByClassName("container-3baos1")[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0];
-    var Crx = /(avatars\/)(.*?)(?=\/)/g;
-    var Csrc = CUser.src;
-    var Cuser = Csrc.split(Crx)[2];
-    var Cele = document.getElementById("app-mount").hasAttribute("NFLDIncCurrentUser");
-    if (Cele == false) {
-      document.getElementById("app-mount").setAttribute("NFLDIncCurrentUser", Cuser);
-    }
+    const UserStore = BdApi.findModuleByProps("getCurrentUser");
+
+    const appMount = document.getElementById("app-mount");
+    appMount.classList.add("NFLDInc"); // add NFLDInc class to appmount
+    appMount.setAttribute("NFLDIncCurrentUser", UserStore.getCurrentUser().id); // Add current user to appmount
   }
 
-  observer() {
-    // chat avatar
-    var NFLDarray = document.getElementsByClassName("avatar-1BDn8e");
-    var NFLDarrayLength = NFLDarray.length;
-    var rx = /(avatars\/)(.*?)(?=\/)/g;
-    for (var i = 0; i < NFLDarrayLength; i++) {
-      var src = document.getElementsByClassName("avatar-1BDn8e")[i].src;
-      var user = src.split(rx)[2];
-      var newClassN = document.createElement("div");
-      newClassN.setAttribute('class', 'NFLDIncChatUser')
-      newClassN.setAttribute("NFLDIncChatUser", user);
-      var ele = document.getElementsByClassName("avatar-1BDn8e")[i].parentElement.childNodes[3];
-      if (ele == undefined) {
-        document.getElementsByClassName("avatar-1BDn8e")[i].parentElement.appendChild(newClassN)
-      }
+  addElement(avatarElement, id) {
+    const newSibling = document.createElement("div");
+    newSibling.classList.add("NFLDIncChatUser");
+    newSibling.setAttribute("NFLDIncChatUser", id ? id : avatarElement.src.split("/")[4]);
+    avatarElement.parentElement.append(newSibling);
+  }
+
+  observer(mutation) {
+    if (!mutation.addedNodes.length || !(mutation.addedNodes[0] instanceof Element)) return;
+    const added = mutation.addedNodes[0];
+
+    const avatarInner = added.querySelectorAll(".avatar-VxgULZ");
+    if (avatarInner.length) {
+        // modal user
+        if (avatarInner.length == 1) { 
+            const id = avatarInner[0].src.split("/")[4];
+            const modal = avatarInner[0].closest(".inner-1ilYF7");
+            if (modal) return modal.setAttribute("NFLDIncPopoutUser", id);
+        }
+
+        // For DevilBro or StatusEverywhere
+        for (let i = 0; i < avatarInner.length; i++) {
+            const avatarOuter = avatarInner[i].closest(".avatar-1BDn8e");
+            if (avatarOuter) this.addElement(avatarOuter, avatarInner[i].src.split("/")[4]);
+        }
+        return; // To avoid the stuff below
     }
 
-    // user popout
-    var pOpen = document.getElementsByClassName("modal-3c3bKg").length
-    if (pOpen == 1) {
-      var PUser = document.getElementsByClassName("modal-3c3bKg")[0].children[2].children[0].children[0].children[0].children[0].children[0].children[0].children[0];
-      var Prx = /(avatars\/)(.*?)(?=\/)/g;
-      var Psrc = PUser.src;
-      var Puser = Psrc.split(Prx)[2];
-      var Pele = document.getElementsByClassName("inner-1ilYF7")[0].hasAttribute("NFLDIncPopoutUser");
-      if (Pele == false) {
-        document.getElementsByClassName("inner-1ilYF7")[0].setAttribute("NFLDIncPopoutUser", Puser);
-      }
+    // Normal chat avatars
+    const avatarOuter = added.querySelectorAll(".avatar-1BDn8e");
+    if (avatarOuter.length) {
+        for (let i = 0; i < avatarOuter.length; i++) this.addElement(avatarOuter[i]);
     }
   }
 
   stop() {
-    while (this.patches.length) this.patches.pop()();
+    const appMount = document.getElementById("app-mount");
+    appMount.classList.remove("NFLDInc"); // remove NFLDInc class to appmount
+    appMount.removeAttribute("NFLDIncCurrentUser"); // remove current user to appmount
+
+    const allChatUserElements = document.querySelectorAll(".NFLDIncChatUser");
+    for (let i = 0; i < allChatUserElements.length; i++) allChatUserElements[i].remove();
   }
 }
